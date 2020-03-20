1ac89f2d02735de8d4b8afa76fd010ffb9ae2f93
dhruvs009 <dhruv18230@iiitd.ac.in>
2020-03-21 01:02:36 +0530

    Obtained all images after assumed authorization.

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, Response, json
 import requests
 import os
 from dotenv import load_dotenv
+import urllib.request
+from PIL import Image
+import math
 
 load_dotenv()
 
@@ -13,7 +16,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET'])
 def stitchig():
-    code=request.args.get('code')[:-2]
+    code=request.args.get('code')
     r=requests.post(url='https://api.instagram.com/oauth/access_token', data={
             'client_id':client_id,
             'client_secret':client_secret,
@@ -27,7 +30,27 @@ def stitchig():
     r=requests.get(url='https://graph.instagram.com/'+str(user_id)+'/media?fields=id&access_token='+str(access_token))
     response=r.json()
     data=response['data']
-
+    imgArray=[]
+    width=0
+    height=0
+    for row in data:
+        r=requests.get(url='https://graph.instagram.com/'+str(row['id'])+'?fields=media_url,media_type,children&access_token='+str(access_token))
+        response=r.json()
+        print(response)
+        if response['media_type']=="IMAGE":
+            image=Image.open(urllib.request.urlopen(response['media_url']))
+            size=150, 150
+            im_resized=image.resize(size, Image.ANTIALIAS)
+            imgArray.append(im_resized)
+        elif response['media_type']=="CAROUSEL_ALBUM":
+            for child in response['children']['data']:
+                rtemp=requests.get(url='https://graph.instagram.com/'+str(child['id'])+'?fields=media_url,media_type,children&access_token='+str(access_token))
+                responsetemp=rtemp.json()
+                image=Image.open(urllib.request.urlopen(responsetemp['media_url']))
+                size=150, 150
+                im_resized=image.resize(size, Image.ANTIALIAS)
+                imgArray.append(im_resized)
+    return ''
 
 if __name__=="__main__":
     app.run(debug=True)
